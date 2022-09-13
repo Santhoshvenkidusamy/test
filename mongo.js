4789b9516679fabaaaa75dffd48b5671d3953a94
santhoshvenkidusamy1 <santhoshvenkidusamy@gmail.com>
2022-09-13 10:57:20 +0530

    commited

---
+++ mongo.js
@@ -0,0 +1,49 @@
+const express=require("express");
+const { Db } = require("mongodb");
+const app=express();
+const mongoose   = require('mongoose')
+app.use(express.json());
+mongoose.connect("mongodb+srv://sampple:Santhosh@cluster0.kwktymk.mongodb.net/?retryWrites=true&w=majority",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+},(err)=>{
+    if(!err){
+        console.log("connected to database");
+    }
+    else{
+         console.log("error");
+         
+    }
+})
+const schema= new mongoose.Schema(
+{
+    clgName:String,
+    location:String,
+    studentDetails:
+    {
+        studentName:String,
+        age:Number,
+        subject:Array
+    }
+},
+{ timestamps: true }
+);
+const college =mongoose.model("college",schema);
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to MongoDb." });
+  });
+  
+app.post("/api/post",async (req,res)=>{
+    const data=new college({
+        clgName:req.body.clgName,
+        location:req.body.location,
+        studentDetails:req.body.studentDetails
+    }
+    )
+    const val= await data.save();
+    res.send(val);
+
+})
+app.listen(4000,()=>{
+    console.log("Server running on port 4000")
+})
